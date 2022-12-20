ecb0c760c7d77710c4d360209540aeea680095c2
Harsh Kanjiya <harshkanjiya001@gmail.com>
2022-12-20 10:23:59 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -8,60 +8,20 @@ import Next from "./components/next";
 
 function App() {
   const [value, setValue] = useState({
-    name: "harsh kanjiya",
-    profession: "berojgar",
-    age: "20",
-    phone: "90542 42004",
-    email: "harshkanjiya001@gmail.com",
-    gender: "male",
+    name: "",
+    profession: "",
+    age: "",
+    phone: "",
+    email: "",
+    gender: "",
     address: "",
-    hobbies: ["Html", "css", "js", "react"],
-    skills: ["css", "js", "react"],
-    languages: ["gujrati", "english"],
-    education: [
-      {
-        course: "B Tech IT",
-        place: "P P Savani University",
-        duration: "2020 - 2024",
-      },
-      {
-        course: "HSC",
-        place: "Ashadip Science",
-        duration: "2019 - 2020",
-      },
-    ],
-    links: [
-      { name: "A", link: "a" },
-      { name: "A", link: "a" },
-    ],
-    experiences: [
-      {
-        name: "b",
-        dipscription: "dsdsdsds",
-      },
-      {
-        name: "sdsd",
-        dipscription: "qwertyuiugfds",
-      },
-      {
-        name: "wdfv",
-        dipscription: "rethgbdsdrtjhndfjn",
-      },
-    ],
-    projects: [
-      {
-        name: "b",
-        dipscription: "dsdsdsds",
-      },
-      {
-        name: "sdsd",
-        dipscription: "qwertyuiugfds",
-      },
-      {
-        name: "wdfv",
-        dipscription: "rethgbdsdrtjhndfjn",
-      },
-    ],
+    hobbies: [],
+    skills: [],
+    languages: [],
+    education: [],
+    links: [],
+    experiences: [],
+    projects: [],
     img: [],
   });
   // console.log('value.lang :>> ', value.links);
